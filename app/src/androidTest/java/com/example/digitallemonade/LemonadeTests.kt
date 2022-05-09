e58488203257bbf4d3ce505763ee90fc78b0f888
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-09 09:11:45 +0200

    - ui test clicking leads to squeeze

--- app/src/androidTest/java/com/example/digitallemonade/LemonadeTests.kt
+++ app/src/androidTest/java/com/example/digitallemonade/LemonadeTests.kt
@@ -1,6 +1,9 @@
 package com.example.digitallemonade
 
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.Before
@@ -19,6 +22,12 @@ class LemonadeTests : BaseTest() {
   @Test
   fun test_initial_state(){
     testState(R.string.click_to_select_a_lemon, R.drawable.lemon_tree)
+  }
+
+  @Test
+  fun test_picking_lemon_proceeds_to_squeeze(){
+    onView(withId(R.id.imageButton)).perform(click())
+    testState(R.string.click_to_juice_a_lemon, R.drawable.lemon_squeeze)
   }
 
 }
